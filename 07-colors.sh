858fcc5c3971a55b26c55b816a33dafd49992f86
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-02-23 21:37:41 +0530

    practice

--- 07-colors.sh
+++ 07-colors.sh
@@ -3,10 +3,10 @@
 userid=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
-R= "\e[31m"
-B= "\e[32m"
-Y= "\e[33m"
-N= "\e[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
  if [ $userid -ne 0 ]; then
    echo "$R please run this script with root access $N" | tee -a $LOGS_FILE
@@ -27,7 +27,7 @@ VALIDATE(){
 for package in $@
 do 
 
-  dnf list installed $package -y &>>$LOGS_FILE
+  dnf list installed $package  &>>$LOGS_FILE
     if [ $? -ne 0 ]; then
     echo "$package not installed instaling now"
     dnf install $package -y &>>$LOGS_FILE
