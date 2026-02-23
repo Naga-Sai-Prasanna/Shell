a7c08da901bf48211e92d6de352eac89bbd13d62
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-02-23 22:01:30 +0530

    practice

--- 09-trap.sh
+++ 09-trap.sh
@@ -1,5 +1,54 @@
+# #!/bin/bash
+# set -e #ERR
+# trap 'echo "there is an error in $LINENO ,command; $BASH_COMMAND"' ERR
+# echo "hello world"
+# echoo "hi" 
+
+
+
+
+#remove trap
+
 #!/bin/bash
+
 set -e #ERR
 trap 'echo "there is an error in $LINENO ,command; $BASH_COMMAND"' ERR
-echo "hello world"
-echoo "hi" 
+
+
+
+userid=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+ if [ $userid -ne 0 ]; then
+   echo -e "$R please run this script with root access $N" | tee -a $LOGS_FILE
+   exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+# VALIDATE(){
+#     if [ $1 -ne 0 ]; then
+#     echo -e "$2......$R failure $N" | tee -a $LOGS_FILE
+#     else
+#     echo -e "$2.....$G success $N" | tee -a $LOGS_FILE
+#     fi
+}
+
+
+for package in $@
+do 
+
+  dnf list installed $package  &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+    echo "$package not installed instaling now"
+    dnf install $package -y &>>$LOGS_FILE
+    #VALIDATE $? "$package installation"
+    else
+    echo -e "$package is already installed $Y SKIPPING $N" | tee -a $LOGS_FILE
+    fi
+done 
