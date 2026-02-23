17e3bd9ab3cde190d44276ea9f23876f75e8df2d
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-02-23 18:45:31 +0530

    conditions practice

---
+++ 03-conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+number=$1
+if [ $number -gt 20 ]; then
+   echo "num is greater than 20"
+
+ ifelse [ $number == 20 ]; then
+    echo "num is equal to  20"
+
+fi
+  else
+   echo "num is less than 20"
