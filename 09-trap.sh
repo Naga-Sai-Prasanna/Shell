a0e16915fc66ce24f9a6be71b973ea6e4dfcf9df
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-02-23 21:58:26 +0530

    practice

--- 09-trap.sh
+++ 09-trap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e #ERR
-trap 'echo "there is an error in $LINENO,command: $BASH_COMMAND"' ERR
+trap 'echo "there is an error in $LINENO ,command; $BASH_COMMAND"' ERR
 echo "hello world"
 echoo "hi" 
