b189d4ae33a438aae85986108ca50caa8f3a0984
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-02-24 19:45:05 +0530

    test

--- test.sh
+++ test.sh
@@ -59,22 +59,22 @@ LOGS_FILE="$LOGS_FOLDER/$0.log"
 
  if [ $userid -ne 0 ]; then
    echo "please run this script with root access" | tee -a $LOGS_FILE
-   exit 1
+  
 fi
 
 mkdir -p $LOGS_FOLDER
 
 
-# VALIDATE(){
-#     if [ $1 -ne 0 ]; then
-#     echo "$2......failure". | tee -a $LOGS_FILE
-#     else
-#     echo "$2.....success". | tee -a $LOGS_FILE
-#     fi
-# }
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+    echo "$2......failure". | tee -a $LOGS_FILE
+    else
+    echo "$2.....success". | tee -a $LOGS_FILE
+    fi
+}
 
-# dnf install nginx -y &>>$LOGS_FILE | tee -a $LOGS_FILE
-# VALIDATE $? "installing nginx"  | tee -a $LOGS_FILE
+dnf install nginx -y &>>$LOGS_FILE | tee -a $LOGS_FILE
+VALIDATE $? "installing nginx"  | tee -a $LOGS_FILE
 
-# dnf remove nginx -y &>>$LOGS_FILE | tee -a $LOGS_FILE
-# VALIDATE $? "removing nginx" | tee -a $LOGS_FILE
+dnf remove nginx -y &>>$LOGS_FILE | tee -a $LOGS_FILE
+VALIDATE $? "removing nginx" | tee -a $LOGS_FILE
